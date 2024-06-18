ef3fddafb1e34b97c5e7cb1af73deaad3b18aa60
Saúl Cabrera <saulecabrera@gmail.com>
2024-06-18 18:52:15 -0400

    Extract lib

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use brotli::BrotliDecompress;
+use std::io::Cursor;
+use std::path::PathBuf;
+use wasmparser::{Parser, Payload::*};
+
+static CUSTOM_SECTION: &'static str = "javy_source";
+
+pub fn extract(path: &PathBuf) -> Result<Option<Vec<u8>>> {
+    let bytes = std::fs::read(&path)?;
+    let parser = Parser::new(0);
+    let mut contents: Vec<u8> = vec![];
+
+    for payload in parser.parse_all(&bytes) {
+        match payload? {
+            CustomSection(reader) => {
+                if reader.name() == CUSTOM_SECTION {
+                    let compressed: Vec<u8> = reader.data().into();
+                    let mut cursor = Cursor::new(compressed);
+                    BrotliDecompress(&mut cursor, &mut contents)?;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    if contents.len() > 0 {
+        Ok(Some(contents))
+    } else {
+        Ok(None)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,17 @@
 //! ```rust
 //! cargo run -- /path/to/module.wasm
 //! ```
-use anyhow::{bail, Result};
-use brotli::BrotliDecompress;
-use std::io::Cursor;
-use wasmparser::{Parser, Payload::*};
 
-static CUSTOM_SECTION: &'static str = "javy_source";
+use anyhow::Result;
+use jjv::extract;
 
 fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let path = args[1].clone();
-    let bytes = std::fs::read(&path)?;
-    let parser = Parser::new(0);
-    let mut found = false;
-
-    for payload in parser.parse_all(&bytes) {
-        match payload? {
-            CustomSection(reader) => {
-                if reader.name() == CUSTOM_SECTION {
-                    let compressed: Vec<u8> = reader.data().into();
-                    let mut cursor = Cursor::new(compressed);
-                    BrotliDecompress(&mut cursor, &mut std::io::stdout())?;
-                    found = true;
-                }
-            }
-            _ => {}
-        }
-    }
-
-    if !found {
-        bail!("{CUSTOM_SECTION} not found");
+    if let Ok(Some(contents)) = extract(&path.into()) {
+        println!("{}", String::from_utf8(contents)?);
+    } else {
+        println!("No JavaScript source found");
     }
 
     Ok(())
